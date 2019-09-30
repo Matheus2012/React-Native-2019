150e6a6f6034b5d95e8fffbfccbd7599ae5056ea
Matheus2012 <matheus_alves0101@outlook.com>
2019-09-30 19:31:34 -0300

    Finalizado módulo de recuperação de senha

--- VanFinderServer/src/app/controllers/authController.js
+++ VanFinderServer/src/app/controllers/authController.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const authConfig = require('../../config/auth')
 const User = require('../models/User')
+const crypto = require('crypto')
+const mailer = require('../../modules/mailer')
 
 const router = express.Router()
 
@@ -20,8 +22,8 @@ router.post('/register', async (req, res) => {
         const user = await User.create(req.body)
         user.password = undefined
         return res.send({
-             user,
-             token: generateToken({ id: user.id })
+            user,
+            token: generateToken({ id: user.id })
         })
 
     } catch (err) {
@@ -40,9 +42,67 @@ router.post('/authenticate', async (req, res) => {
     user.password = undefined
 
     res.send({
-         user,
-         token: generateToken({ id: user.id }),
-     })
+        user,
+        token: generateToken({ id: user.id }),
+    })
 })
 
+router.post('/forgot_password', async (req, res) => {
+    const { email } = req.body
+    try {
+        const user = await User.findOne({ email })
+        const token = crypto.randomBytes(20).toString('hex')
+        const now = new Date()
+        now.setHours(now.getHours() + 1)
+        if (!user)
+            return res.status(400).send({ error: 'User not found' })
+        await User.findByIdAndUpdate(user.id, {
+            '$set': {
+                passwordResetToken: token,
+                passwordResetExpires: now,
+            }
+        })
+        console.log(token,now)
+        mailer.sendMail({
+            to:email,
+            from:'matheus_alves0101@outlook.com',
+            template:'auth/forgot_password',
+            context:{token}
+        },(err)=>{
+            if(err)
+                return res.status(400).send({error:'Cannot send forgot password email'})
+
+            return res.send()
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({ error: 'Erro on forgot password, try again' })
+    }
+})
+
+router.post('/reset_password', async(req,res) =>{
+    const {email,token,password} = req.body
+
+    try{
+        const user = await User.findOne({email}).select('+passwordResetToken passwordResetExpires')
+        if(!user)
+        return res.status(400).send({error: 'User not found'})
+
+        if(token !== user.passwordResetToken)
+        return res.status(400).send({error: 'Token invalid'})
+
+        const now = new Date()
+
+        if(now > user.passwordResetExpires)
+            return res.status(400).send({error: 'Token expired, generate a new one'})
+
+            user.password = password;
+
+            await user.save()
+            res.send()
+    }catch(error){
+        res.send(400).send({error: 'Cannot reset password, try again'})
+    }
+})
 module.exports = app => app.use('/auth', router)
--- VanFinderServer/src/index.js
+++ VanFinderServer/src/index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-require('./controllers/index')(app)
+require('./app/controllers/index.js')(app)
 
 app.get('/', (rep, res) => {
     res.send('OK')
